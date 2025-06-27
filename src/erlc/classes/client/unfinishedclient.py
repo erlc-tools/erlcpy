19ba7ea3f7592e90440d1a8f30d374e89dd06c99
Fin M <fin@mace.law>
2025-06-27 13:53:34 -0400

    add .config() to UnfinishedClient

--- src/erlc/classes/client/unfinishedclient.py
+++ src/erlc/classes/client/unfinishedclient.py
@@ -2,4 +2,9 @@ class UnfinishedClient:
     @property
     def options(self):
         print(f"FATAL: A call for options on a UnfinishedClient was called. This means you didnt call Client.config")
+        return
+    
+    def config(self):
+        print(f"FATAL: A call for config on a UnfinishedClient was called. This means you are calling config on the wrong thing.")
+        return
 
