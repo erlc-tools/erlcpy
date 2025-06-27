2043e0e6e4229cfb62e6ba7b73469b810b2adff0
Fin M <fin@mace.law>
2025-06-27 13:38:07 -0400

    create client class

--- src/erlc/classes/client/__init__.py
+++ src/erlc/classes/client/__init__.py
@@ -1,3 +1,10 @@
 class Client():
-    def __init__(self):
-        ...
+    def __init__(self, options: dict):
+        if not isinstance(options, dict):
+            raise TypeError(f"Expected dict for parameters options. Got {type(options)}")
+            return
+        
+        self.options = options
+    
+    def config(self):
+        return self.options
