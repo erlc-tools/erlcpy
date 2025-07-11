06b2753990072e286314f9e1e905b116169319c3
Fin M <fin@mace.law>
2025-07-10 23:26:47 -0400

    add debug logging

--- src/erlc/logging/__init__.py
+++ src/erlc/logging/__init__.py
@@ -1,2 +1,10 @@
+debug: bool = True
+if debug:
+    from icecream import ic
+
+
 def error(content):
     print(f"ERROR: {content}")
+def debug(content):
+    if debug:
+        ic("ERLC-PY DEBUG", content)
