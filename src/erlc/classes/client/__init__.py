fe9a70838996fc4c90f52303c0481d933b48358d
Fin M <fin@mace.law>
2025-07-10 23:08:40 -0400

    create printDebug function

--- src/erlc/classes/client/__init__.py
+++ src/erlc/classes/client/__init__.py
@@ -1,6 +1,10 @@
+from erlc.methods.printDebug import printDebug as printDebugORIGIN
 class Client():
     def __init__(self, globalToken: bool | None):
         self.globalToken = globalToken
+    
+    def printDebug(self):
+        printDebugORIGIN(self)
     
 
 exports = [Client]
---
+++ src/erlc/methods/printDebug/__init__.py
@@ -0,0 +1,5 @@
+from erlc.classes.client import Client
+from icecream import ic
+
+def printDebug(client: Client):
+    ic(client)
