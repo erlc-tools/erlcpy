8600bb0f3b434c4ad5a6cf740e396f8b35a3b8aa
Fin M <fin@mace.law>
2025-07-10 23:32:14 -0400

    add playerCount func to client class

--- src/erlc/classes/client/__init__.py
+++ src/erlc/classes/client/__init__.py
@@ -1,10 +1,14 @@
 from erlc.methods.printDebug import printDebug as printDebugORIGIN
+from erlc.methods.playerCount import playerCount as playerCountORIGIN
 class Client():
     def __init__(self, globalToken: bool | None):
         self.globalToken = globalToken
     
     def printDebug(self):
         printDebugORIGIN(self)
+    
+    def playerCount(self, key: str):
+        playerCountORIGIN(self, key)
     
 
 exports = [Client]
