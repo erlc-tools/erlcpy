84a143cc8994d13fd767e8dc17dfd1195ea103bb
Fin M <fin@mace.law>
2025-07-10 23:29:57 -0400

    add debug warning to __init__ file

--- src/erlc/__init__.py
+++ src/erlc/__init__.py
@@ -1,6 +1,9 @@
 from erlc.classes.client import ( Client )
+from erlc.logging import debug
 
 
+debug("Enabling debug is dangerous!!!", "It may reveal keys, ratelimit authorization keys, etc.",
+      "Make sure you aren't ssh'ing or screen sharing!")
 
 exports = [Client] # change this instead of __all__!
 __all__ = exports
