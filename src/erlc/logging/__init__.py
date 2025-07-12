c1bd99525c18c93a0afe64a288acd627e84f0e70
Fin M <fin@mace.law>
2025-07-12 18:13:31 -0400

    make logging now oop

--- src/erlc/logging/__init__.py
+++ src/erlc/logging/__init__.py
@@ -1,10 +1,12 @@
-debug: bool = True
-if debug:
-    from icecream import ic
-
-
-def error(content):
-    print(f"ERROR: {content}")
-def debug(*args):
-    if debug:
-        ic("ERLC-PY DEBUG", args)
+class Logger():
+    def __init__(self, debug: bool = False):
+        if debug:
+            from icecream import ic
+            self._ic = ic # allow for use outside of the class
+    
+    def error(self, content):
+        print(f"ERROR: {content}")
+        
+    def debug(self, *args):
+        if self.debug:
+            self._ic("ERLC-PY DEBUG", args)
