c1b3a1daab894d7a34f26a186b3215cfeb722198
Fin M <fin@mace.law>
2025-07-12 18:19:36 -0400

    move client.logger.debug to not be static, and be whatever the user says

--- src/erlc/classes/client/__init__.py
+++ src/erlc/classes/client/__init__.py
@@ -2,10 +2,11 @@ from erlc.methods.printDebug import printDebug as printDebugORIGIN
 from erlc.methods.playerCount import playerCount as playerCountORIGIN
 from erlc.logging import Logger
 class Client():
-    def __init__(self, globalToken: bool | None):
+    def __init__(self, globalToken: bool | None,
+                 debug: bool = False):
         self.globalToken = globalToken
         
-        self.logger = Logger(debug=False) # later on change this to be an arg
+        self.logger = Logger(debug=debug)
     
     def printDebug(self):
         printDebugORIGIN(self)
