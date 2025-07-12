a0a3869b9283ac6895de1f548405a3848c38751d
Fin M <fin@mace.law>
2025-07-12 18:33:35 -0400

    update .create to set self.raw to args like it should

--- src/erlc/types/BaseType/__init__.py
+++ src/erlc/types/BaseType/__init__.py
@@ -1,8 +1,10 @@
 class BaseType:
-    raw: str | list | dict | int | bool # Original input
+    raw: any # Original input
     
     class Value: # This will be the actual value of the thing
         ...
     
     def create(self, *args):
         print("ERLC-PY WARNING: BaseType.create was called.")
+        
+        self.raw = args
