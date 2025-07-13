e62b464174b7a1ec56bbb43294d29d3ce736f133
Fin M <fin@mace.law>
2025-07-13 11:36:31 -0400

    rename debug var to debugEnabled + add it to self

--- src/erlc/logging/__init__.py
+++ src/erlc/logging/__init__.py
@@ -1,6 +1,8 @@
 class Logger():
     def __init__(self, debug: bool = False):
-        if debug:
+        self.debugEnabled = debug
+        
+        if self.debugEnabled:
             from icecream import ic
             self._ic = ic # allow for use outside of the class
     
@@ -8,5 +10,5 @@ class Logger():
         print(f"ERROR: {content}")
         
     def debug(self, *args):
-        if self.debug:
+        if self.debugEnabled:
             self._ic("ERLC-PY DEBUG", args)
